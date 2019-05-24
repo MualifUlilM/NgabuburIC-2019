e748b70bacf72aeab95eff5008e31d3f8f057db1
mualif <36094387+mualif@users.noreply.github.com>
2019-05-24 15:30:48 +0700

    Add files via upload

---
+++ ngabuburic 2019/face-train.py
@@ -0,0 +1,49 @@
+import os
+import cv2
+import numpy as np
+from PIL import Image
+import pickle
+
+BASE_DIRS = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(BASE_DIRS, 'images')
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+current_id = 0
+label_ids = {}
+x_trains = []
+y_labels = []
+
+for root, dirs, files in os.walk(image_path):
+    for file in files:
+        if file.endswith('jpg') or file.endswith('jpg'):
+            path = os.path.join(root, file)
+            label = os.path.basename(root).replace(" ", "-").lower()
+            print(label, path)
+
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+
+            id_ = label_ids[label]
+            print(label_ids)
+
+            pil_image = Image.open(path).convert("L")
+            size = (550, 550)
+            final_image = pil_image.resize(size, Image.ANTIALIAS)
+            image_array = np.array(final_image, "uint8")
+            faces = face_cascade.detectMultiScale(image_array, 1.3, 5)
+
+            for(x, y, w, h) in faces:
+                roi = image_array[x:x+w, y:y+h]
+                x_trains.append(roi)
+                y_labels.append(id_)
+
+print(y_labels)
+print(x_trains)
+with open("label.pickle", "wb") as f:
+    pickle.dump(label_ids, f)
+
+recognizer.train(x_trains, np.array(y_labels))
+recognizer.save("trainer.yml")
---
+++ ngabuburic 2019/faces.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import pickle
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainer.yml")
+
+labels = {"person_name" : 1}
+
+with open("label.pickle", "rb") as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k, v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+
+        roi_color = frame[x:x+w, y:y+h]
+        roi_gray = gray[x:x+w, y:y+h]
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 45:
+            print(id_)
+
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+
+        image_items = 'image.jpg'
+        cv2.imwrite(image_items, roi_color)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
